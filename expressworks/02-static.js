f20ed58f37ee5c35d27174b971eb9b1e22f57783
Hierophantos <silvergodofgold@gmail.com>
2015-12-11 19:56:00 -0800

    git ignore

---
+++ expressworks/02-static.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    app     = express(),
+    path    = require('path');
+
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+
+app.listen(process.argv[2] || 8080);
+
+
+
+
+
+
+
+
